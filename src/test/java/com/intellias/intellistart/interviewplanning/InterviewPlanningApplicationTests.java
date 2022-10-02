3f53cb487f07a986fb0c1ee13c84be73515dd473
Mariia Sirenko <mariiasirenko2@gmail.com>
2022-10-02 20:59:44 +0300

    Initial commit

---
+++ src/test/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellias.intellistart.interviewplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPlanningApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
